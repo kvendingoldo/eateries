07c30fc18615e166e1f024d6acd94754edcc6523
Alex Sharov <kvendingoldo@yandex.ru>
2017-06-24 23:54:23 +0400

    Add maven structure

---
+++ eateries-service/src/main/java/com/kvendingoldo/eateries/service/Main.java
@@ -0,0 +1,14 @@
+package com.kvendingoldo.eateries.service;
+
+/**
+ * @author Alexander Sharov
+ * @email kvendingoldo@yandex.ru
+ * @since 24.06.17
+ */
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("mock");
+    }
+}
